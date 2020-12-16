1c0d11afa3ed2d2a561287370268d35272e2c587
khanhhoaa19 <55345757+khanhhoaa19@users.noreply.github.com>
2020-12-16 22:43:38 +0700

    update

--- deploy/app.py
+++ deploy/app.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from flask import Flask, request, render_template
 
@@ -5,11 +6,47 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
 @app.route('/', methods=['POST'])
 
-@app.route('/gui')
-def index():
-    return render_template('index1.html')
+def webhook():
+    data = request.get_json()
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):
+
+                    sender_id = messaging_event["sender"]["id"]
+                    recipient_id = messaging_event["recipient"]["id"]
+                    message_text = messaging_event["message"]["text"]
+
+                    response_ = response(message_text, sender_id)
+                    send_message(sender_id, response_)
+
+                if messaging_event.get("delivery"):
+                    pass
+
+                if messaging_event.get("optin"):
+                    pass
+
+                if messaging_event.get("postback"):
+                    pass
+
+    return "ok", 200
+
+# @app.route('/gui')
+# def index():
+#     return render_template('index1.html')
 
 if __name__ == '__main__':
     app.run()
