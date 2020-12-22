a414423b25b6bdb97490e6dace3ae2a18ecd8658
khanhhoaa19 <55345757+khanhhoaa19@users.noreply.github.com>
2020-12-22 15:14:27 +0700

    update

--- deploy/app.py
+++ deploy/app.py
@@ -1,13 +1,8 @@
 import os
-import sys
-
 import requests
 from flask import Flask, request
 
-import nltk
-nltk.download('punkt')
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
+from underthesea import word_tokenize
 
 import numpy as np
 import random
@@ -16,7 +11,6 @@ from keras.models import load_model
 
 from process.preProcess import processing
 
-# import our chat-bot intents file
 import json
 with open('intents.json') as json_data:
     intents = json.load(json_data)
@@ -35,41 +29,31 @@ for line in file_Stop_word:
 
 ignore_words = list(stopWords)
 
-# loop through each sentence in our intents questions
+
 for intent in intents['intents']:
     for question in intent['questions']:
-        # tokenize each word in the sentence
-        w = nltk.word_tokenize(question)
-        # add to our words list
+        w = word_tokenize(question)
         words.extend(w)
-        # add to documents in our corpus
         documents.append((w, intent['tag']))
-        # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# stem and lower each word and remove duplicates
-words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+
+words = [w.lower() for w in words if w not in ignore_words if len(w) != 1]
 words = sorted(list(set(words)))
 
-# remove duplicates
 classes = sorted(list(set(classes)))
 
 model = load_model('model.h5')
 
 def clean_up_sentence(sentence):
-    # tokenize the question
     sentence_words = processing(sentence).split()
-    # stem each word
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    sentence_words = [word.lower() for word in sentence_words if len(word)>1]
     return sentence_words
 
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 def bow(sentence, words, show_details=False):
-    # tokenize the question
     sentence_words = clean_up_sentence(sentence)
-    # bag of words
     bag = [0] * len(words)
     for s in sentence_words:
         for i, w in enumerate(words):
@@ -100,6 +84,7 @@ def classify(sentence):
         return_list.append((classes[r[0]], r[1]))
     return return_list
 
+
 def response(sentence, userID, show_details=False):
     results = classify(sentence)
     print('Result:', results)
@@ -107,11 +92,11 @@ def response(sentence, userID, show_details=False):
         while results:
             for i in intents['intents']:
                 if i['tag'] == results[0][0]:
-                    if 'context_set' in i:
-                        if show_details: print('context:', i['context_set'])
-                        context[userID] = i['context_set']
+                    if 'contexture_lv1' in i:
+                        if show_details: print('context:', i['contexture_lv2'])
+                        context[userID] = i['contexture_lv1']
                     if not 'context_filter' in i or \
-                            (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                            (userID in context and 'contexture_lv1' in i and i['contexture_lv1'] == context[userID]):
                         if show_details: print('tag:', i['tag'])
                         return (random.choice(i['answers']))
             results.pop(0)
@@ -128,6 +113,7 @@ def verify():
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
+
 
 @app.route('/', methods=['POST'])
 
@@ -158,6 +144,7 @@ def webhook():
                     pass
 
     return "ok", 200
+
 
 def send_message(recipient_id, message_text):
     print("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
