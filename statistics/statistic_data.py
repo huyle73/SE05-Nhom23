af03ff9c83397c18acadda73cb59a31475d6dbe3
huyle73 <54856997+huyle73@users.noreply.github.com>
2021-01-17 15:05:22 +0700

    update

--- statistics/statistic_data.py
+++ statistics/statistic_data.py
@@ -1,6 +1,6 @@
 import json
-import csv
 import pandas as pd
+from IPython.display import HTML
 with open('../deploy/intents.json') as json_data:
     intents = json.load(json_data)
 # Các tags có câu hỏi và câu trả lời
@@ -21,16 +21,29 @@ for intent in intents["intents"]:
         tags_NQA.append(intent["tag"])
 
 def show_details(tags_QA,tags_NQA):
-    print("Các nhãn có câu hỏi và câu trả lời: ")
+    # print("Các nhãn có câu hỏi và câu trả lời: ")
     df_sample = pd.DataFrame(data=tags_QA,columns=["Tags","Number of questions","Number of answers"])
-    print(df_sample)
-    print(df_sample.sum(axis = 0,numeric_only=True))
+    df_sample.loc['Total'] = df_sample.sum(numeric_only=True, axis=0)
+    # print(df_sample)
+    print(type((df_sample.sum(axis = 0,numeric_only=True))))
+    total = df_sample.sum(axis = 0,numeric_only=True)
+    # print(total)
 
-    print()
-    print("Các nhãn không có câu hỏi và câu trả lời: ")
-    df_sample = pd.DataFrame(data=tags_NQA,columns=["Tags"])
-    print(df_sample)
+    html = df_sample.to_html()
+    text_file = open("have_qa.html", "w", encoding="utf-8")
+    text_file.write(html)
+    text_file.close()
+    HTML(df_sample.to_html(classes='table table-striped'))
+
+
+    # print()
+    # print("Các nhãn không có câu hỏi và câu trả lời: ")
+    df_sample1 = pd.DataFrame(data=tags_NQA,columns=["Tags"])
+    html1 = df_sample1.to_html()
+    text_file1 = open("no_qa.html", "w", encoding="utf-8")
+    text_file1.write(html1)
+    text_file1.close()
+    HTML(df_sample.to_html(classes='table table-striped'))
+
 
 show_details(tags_QA,tags_NQA)
-
-
