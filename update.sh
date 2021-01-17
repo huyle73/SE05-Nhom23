19450677c894d798fe7630c31401ec0d60e9c036
dao <mrspam1906@gmail.com>
2021-01-18 06:23:52 +0700

    update

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ cd deploy
 echo "Bạn chỉ có thể đăng nhập 1 lần, vì thế hãy nhập đúng tài khoản và mật khẩu Heroku nhé!"
 heroku login -i
 
-#git add .
-#git commit -am "update"
-#git push heroku master
+git add .
+git commit -am "update"
+git push heroku master
 
 echo "update successfully"
